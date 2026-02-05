45dbfcebaa72826a0e23760720ff23b38948bdde
Noé Plantier <plantiernoe50@gmail.com>
2026-02-05 12:44:30 +0800

    Bug Fixing / View Optimized With Expo

--- backend/server.py
+++ backend/server.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "server:app",
         host="0.0.0.0",
-        port=5000,
+        port=5001,
         reload=True,
         log_level="info"
     )
